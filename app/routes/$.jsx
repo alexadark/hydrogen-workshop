386cd595eda4b35716c120ff75778aa7c5608225
Alexa spalato <alexaspalato@gmail.com>
2023-09-25 12:11:07 +0200

    splat route

---
+++ app/routes/$.jsx
@@ -0,0 +1,35 @@
+import {useLoaderData} from '@remix-run/react';
+import {json} from '@shopify/remix-oxygen';
+
+import {
+  getStoryblokApi,
+  useStoryblokState,
+  StoryblokComponent,
+} from '@storyblok/react';
+
+const PageRoute = () => {
+  let {story} = useLoaderData();
+  story = useStoryblokState(story);
+
+  return (
+    <>
+      <StoryblokComponent blok={story.content} />
+    </>
+  );
+};
+
+export default PageRoute;
+
+export const loader = async ({params}) => {
+  let slug = params['*'] ?? 'home';
+
+  let sbParams = {
+    version: 'draft',
+  };
+
+  let {data} = await getStoryblokApi().get(`cdn/stories/${slug}`, sbParams);
+
+  return json({
+    story: data?.story,
+  });
+};
